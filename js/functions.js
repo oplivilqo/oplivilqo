ed490f8c56b535436fe4f48f21710762160cd3cd
BrightSu <14856499+Blue-Roar@users.noreply.github.com>
2025-11-28 16:01:53 +0800

    feat: read config from yaml

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,28 @@
+const text_postion = [728,355]; // 文本范围起始位置
+const text_over = [2339,800];  // 文本范围右下角位置
+const shadow_offset = [2, 2]; // 阴影偏移量
+const shadow_color = [0, 0, 0]; // 黑色阴影
+
+// 角色配置字典
+var mahoshojo = {};
+// 角色文字配置字典
+var text_configs = {};
+
 function initBackgrounds() {
     // 渲染背景选择
     let backgrounds_div = $("#backgrounds_div");
     backgrounds_div.empty();
-    for (const [key, value] of Object.entries(backgrounds)) {
+
+    // 背景列表
+    const backgrounds = ["c1","c2","c3","c4","c5","c6","c7","c8","c9","c10","c11","c12","c13","c14","c15","c16"];
+
+    for (const key of backgrounds) {
         let bg_html = `
-        <div class="col-3">
+        <div class="col-6 col-md-3">
             <label class="form-imagecheck mb-2">
-                <input name="background" type="radio" value="${key}" class="form-imagecheck-input" onchange="updateCanvas()"${key=="c15" ? " checked" : ""}/>
+                <input name="background" type="radio" value="${key}" class="form-imagecheck-input" onchange="updateCanvas()" onclick="updateCanvas()"${key=="c15" ? " checked" : ""}/>
                 <span class="form-imagecheck-figure">
-                    <img class="form-imagecheck-image" src="./assets/background/${key}.png" alt="${value.name}" title="${value.name}" data-bs-toggle="tooltip"/>
+                    <img class="form-imagecheck-image" src="./assets/background/${key}.png"/>
                 </span>
             </label>
         </div>
@@ -24,9 +38,9 @@ function initCharacters() {
         let char_html = `
         <div class="col-auto">
             <label class="form-imagecheck mb-2">
-                <input name="character" type="radio" value="${key}" class="form-imagecheck-input" onchange="initEmotions('${key}');updateCanvas()"${key=="sherri" ? " checked" : ""}/>
+                <input name="character" type="radio" value="${key}" class="form-imagecheck-input" onchange="initEmotions('${key}');updateCanvas()" onclick="initEmotions('${key}');updateCanvas()"${key=="sherri" ? " checked" : ""}/>
                 <span class="form-imagecheck-figure">
-                    <span class="avatar avatar-xl" style="background-image: url('./assets/chara/${key}/${key} (1).png')" title="${value.name}" data-bs-toggle="tooltip"></span>
+                    <span class="avatar avatar-xl" style="background-image: url('./assets/chara/${key}/${key} (1).png')" title="${value.full_name}" data-bs-toggle="tooltip"></span>
                 </span>
             </label>
         </div>
@@ -42,7 +56,7 @@ function initEmotions(character) {
         let emotion_html = `
         <div class="col-auto">
             <label class="form-imagecheck mb-2">
-                <input name="emotion" type="radio" value="${i}" class="form-imagecheck-input" onchange="updateCanvas()" />
+                <input name="emotion" type="radio" value="${i}" class="form-imagecheck-input" onchange="updateCanvas()" onclick="updateCanvas()"/>
                 <span class="form-imagecheck-figure">
                     <img class="form-imagecheck-image avatar avatar-xl" src="./assets/chara/${character}/${character} (${i}).png"/>
                 </span>
@@ -56,14 +70,17 @@ function initEmotions(character) {
 function updateCanvas() {
     let canvas = $('#canvas')[0];
     let ctx = canvas.getContext("2d");
+
+    // 清空画布
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     
     if ($('input[name="background"]:checked')) ctx.drawImage($('input[name="background"]:checked').next().children()[0], 0, 0, canvas.width, canvas.height);
     
     let character = $('input[name="character"]:checked').val();
     if (character) {
         if ($('input[name="emotion"]:checked')) ctx.drawImage($('input[name="emotion"]:checked').next().children()[0], 0, 134);
-        for (const [key, value] of Object.entries(text_configs_dict[character])) {
-            ctx.font = `${value.font_size}px 华文中宋`;
+        for (const [key, value] of Object.entries(text_configs[character])) {
+            ctx.font = `${value.font_size}px font3`;
 
             ctx.fillStyle = `rgb(${shadow_color[0]}, ${shadow_color[1]}, ${shadow_color[2]})`;
             ctx.fillText(value.text, value.position[0] + shadow_offset[0], value.font_size + value.position[1] + shadow_offset[1]);
@@ -76,7 +93,7 @@ function updateCanvas() {
     let text = $('input[name="text"]').val();
     if (text) {
         let text_font_size = parseInt($('input[name="text_font_size"]').val());
-        ctx.font = `${text_font_size}px 华文中宋`;
+        ctx.font = `${text_font_size}px ${mahoshojo[character].font}`;
         
         // 自动换行绘制
         const maxWidth = text_over[0] - text_postion[0];
@@ -139,8 +156,110 @@ function downloadImage() {
         saveAs(blob, `魔裁文本框表情-${Date.now()}.png`);
     });
 }
-function init() {
-    initBackgrounds();
-    initCharacters();
-    initEmotions('sherri');
+function checkConfigs(direct=false, chara_meta_yaml="", text_configs_yaml="") {
+    if (direct) {
+        if (chara_meta_yaml == "" || text_configs_yaml == "") {
+            $.when(
+                $.get("./config/chara_meta.yml", function(data) {
+                    chara_meta_yaml = data;
+                }).fail(function(e) {
+                    console.warn("读取 chara_meta.yml 失败：", e);
+                    checkConfigs(false);
+                }),
+                $.get("./config/text_configs.yml", function(data) {
+                    text_configs_yaml = data;
+                }).fail(function(e) {
+                    console.warn("读取 text_configs.yml 失败：", e);
+                    checkConfigs(false);
+                })
+            ).done(function() {
+                if (chara_meta_yaml == "" || text_configs_yaml == "") {
+                    console.info("未能通过 YAML 直读加载配置，尝试从本地存储加载。");
+                    checkConfigs(false);
+                } else {
+                    checkConfigs(true, chara_meta_yaml, text_configs_yaml);
+                }
+            });
+        }
+    } else {
+        chara_meta_yaml = localStorage.getItem("manosaba_chara_meta") ?? "";
+        text_configs_yaml = localStorage.getItem("manosaba_text_configs") ?? "";
+    }
+
+    mahoshojo = chara_meta_yaml ? jsyaml.load(chara_meta_yaml)['mahoshojo'] : {};
+    text_configs = text_configs_yaml ? jsyaml.load(text_configs_yaml)['text_configs'] : {};
+    $('#chara_meta').val(chara_meta_yaml);
+    $('#text_configs').val(text_configs_yaml);
+    if (Object.keys(mahoshojo).length > 0) {
+        for (const [key, value] of Object.entries(mahoshojo)) {
+            value.font = value.font.replace(".ttf", "");
+        }
+        $('#chara_meta-indicator').html(`<span class="text-success">已加载 ${Object.keys(mahoshojo).length} 条${direct ? "（YAML 直读）" : ""}</span>`);
+    } else {
+        $('#chara_meta-indicator').html('<span class="text-danger">未加载</span>');
+    }
+    if (Object.keys(text_configs).length > 0) {
+        $('#text_configs-indicator').html(`<span class="text-success">已加载 ${Object.keys(text_configs).length} 条${direct ? "（YAML 直读）" : ""}</span>`);
+    } else {
+        $('#text_configs-indicator').html('<span class="text-danger">未加载</span>');
+    }
+    
+    $('ul.nav.nav-tabs.card-header-tabs li, ul.nav.nav-tabs.card-header-tabs li > a').removeClass("active");
+    if (Object.keys(mahoshojo).length > 0 && Object.keys(text_configs).length > 0) {
+        initBackgrounds();
+        initCharacters();
+        initEmotions('sherri');
+        $('ul.nav.nav-tabs.card-header-tabs li').removeAttr("hidden");
+        $('ul.nav.nav-tabs.card-header-tabs li:nth-child(2), ul.nav.nav-tabs.card-header-tabs li:nth-child(2) > a').addClass("active");
+        $('.tab-pane').removeClass("active show");
+        $('#tabs-chara').addClass("active show");
+        $('#preview_div').show();
+        if (direct) {
+            $('#configs_upload_div').html("已经通过 YAML 文件直接加载配置，无需上传。");
+        }
+    } else {
+        $('ul.nav.nav-tabs.card-header-tabs li').attr("hidden", true);
+        $('ul.nav.nav-tabs.card-header-tabs li:nth-child(3)').removeAttr("hidden");
+        $('ul.nav.nav-tabs.card-header-tabs li:nth-child(3), ul.nav.nav-tabs.card-header-tabs li:nth-child(3) > a').addClass("active");
+        $('.tab-pane').removeClass("active show");
+        $('#tabs-settings').addClass("active show");
+        $('#preview_div').hide();
+    }
 }
+function resetConfigs() {
+    localStorage.removeItem("manosaba_chara_meta");
+    localStorage.removeItem("manosaba_text_configs");
+    checkConfigs();
+}
+function init() {
+    checkConfigs(true);
+}
+$(document).ready(function() {
+    init();
+    $('input[name="configs"]').on('change', function(event) {
+        const files = event.target.files;
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                const content = e.target.result;
+                try {
+                    const data = jsyaml.load(content);
+                    if (file.name === "chara_meta.yml") {
+                        $('#chara_meta').val(content);
+                        localStorage.setItem("manosaba_chara_meta", content);
+                    } else if (file.name === "text_configs.yml") {
+                        $('#text_configs').val(content);
+                        localStorage.setItem("manosaba_text_configs", content);
+                    } else {
+                        console.info("已跳过未知的配置文件：" + file.name);
+                    }
+                } catch (error) {
+                    console.warn("解析 " + file.name + " 失败：" + error);
+                }
+                checkConfigs();
+            };
+            reader.readAsText(file);
+        }
+    });
+});
